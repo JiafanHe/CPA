7d6e3ff680d48887e4b61cb8da623e70230e2627
JiafanHe <jiafan453@gmail.com>
2021-12-02 22:07:55 -0500

    Modified the about screen

--- AboutScreen.js
+++ AboutScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 import {useValue} from './ValueContext';
+
 
 
 export default function AboutScreen(){
@@ -8,10 +9,38 @@ export default function AboutScreen(){
   const name = currentValue.name;
 
   return(
-    <View>
+    <ScrollView>
+      <View>
 
-        <Text style={{marginTop:40,fontSize:20}}>Hello, I'm {name}. This app is designed for civil engineers.🔥 </Text>
+          <Text style={{marginTop:40,fontSize:20,marginLeft:10}}>Hello, I'm {name}. This app is designed for civil engineers.🔥 </Text>
 
-    </View>
+          <Text style={{marginTop:40,fontSize:20,marginLeft:10,marginBottom:20}}>In real life, the load compositions are extremely complicated. But we can decompose them into
+          differen loads. The formula of each case is listed below.</Text>
+
+          <View style={{flexDirection:'column',alignItems:'center'}}>
+            <Image
+              style={{
+                height:300,
+                width:"90%",
+                resizeMode: 'stretch',
+                marginBottom:0
+              }}
+              source={require('./Image/LoadingCases.png')}
+            />
+
+            <Image
+              style={{
+                height:150,
+                width:"90%",
+                resizeMode: 'stretch',
+                marginTop:0}}
+              source={require('./Image/LoadingCases2.png')}
+            />
+          </View>
+
+
+
+      </View>
+    </ScrollView>
   )
 }
